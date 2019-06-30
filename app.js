4cfeb120e7b0fc3e8ccf5a7aef8fda5062c34686
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-06-30 20:31:53 +0200

    update cors

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var models =  require("./models");
 
 models.sequelize.sync().then(function() {
